d5aed2f04ebe8c1ac3437432a0890936c37bfa28
Giulio Starace <giulio.starace@gmail.com>
2022-04-10 18:14:10 +0200

    rename nli.py to infersent.py

---
+++ models/infersent.py
@@ -0,0 +1,10 @@
+"""Generic InferSent model"""
+import pytorch_lightning as pl
+
+
+class InferSent(pl.LightningModule):
+    """Generic InferSent model"""
+
+    def __init__(self):
+        super().__init__()
+        # TODO
