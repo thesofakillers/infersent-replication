e1f73500532035b3dc84edc243e695caef71b7a4
Giulio Starace <giulio.starace@gmail.com>
2022-04-18 17:43:02 +0200

    implement maxpoolbilstm

--- models/encoder.py
+++ models/encoder.py
@@ -57,12 +57,12 @@ class LSTM(Encoder):
 
     def __init__(self, vocab, word_emb_dim):
         super(LSTM, self).__init__(vocab, word_emb_dim)
-        self.out_dim = 2048
+        self.out_dim = self.hid_dim
         self.lstm = nn.LSTM(word_emb_dim, self.out_dim, batch_first=True)
 
     def forward(self, sent_tuple):
         """
-        Forward pass. Need to pack and pad.
+        Forward pass. Need to pack.
         We are interested in the final hidden state of the LSTM.
         """
         # sent is B x L, where L is max(sent_len), B is batch size; sent_len is B x 1
@@ -79,16 +79,19 @@ class LSTM(Encoder):
 
 
 class BiLSTM(Encoder):
-    """LSTM encoder"""
+    """BiLSTM encoder"""
 
     def __init__(self, vocab, word_emb_dim):
         super(BiLSTM, self).__init__(vocab, word_emb_dim)
+        self.hid_dim = 2048
         self.out_dim = 4096
-        self.bilstm = nn.LSTM(word_emb_dim, 2048, batch_first=True, bidirectional=True)
+        self.bilstm = nn.LSTM(
+            word_emb_dim, self.hid_dim, batch_first=True, bidirectional=True
+        )
 
     def forward(self, sent_tuple):
         """
-        Forward pass. Need to pack and pad.
+        Forward pass. Need to pack.
         We are interested in the final hidden state of the BiLSTM.
         """
         # sent is B x L, where L is max(sent_len), B is batch size; sent_len is B x 1
@@ -99,8 +102,43 @@ class BiLSTM(Encoder):
         packed_emb = nn.utils.rnn.pack_padded_sequence(
             emb, sent_len.squeeze(1), batch_first=True, enforce_sorted=False
         )
-        # h_t is 2 X B x H, where H is self.out_dim
+        # h_t is 2 X B x H, where H is self.hid_dim
         _out, (h_t, _c_t) = self.bilstm(packed_emb)
         # we concatenate the final hidden states of the forward and backward LSTMs
         out = torch.cat((h_t[0], h_t[1]), dim=1)
+        return out
+
+
+class MaxPoolBiLSTM(Encoder):
+    """MaxPoolBiLSTM encoder"""
+
+    def __init__(self, vocab, word_emb_dim):
+        super(MaxPoolBiLSTM, self).__init__(vocab, word_emb_dim)
+        self.hid_dim = 2048
+        self.out_dim = 4096
+        self.bilstm = nn.LSTM(
+            word_emb_dim, self.hid_dim, batch_first=True, bidirectional=True
+        )
+
+    def forward(self, sent_tuple):
+        """
+        Forward pass. Need to pack.
+        We are interested in the final hidden state of the BiLSTM.
+        """
+        # sent is B x L, where L is max(sent_len), B is batch size; sent_len is B x 1
+        sent, sent_len = sent_tuple
+        # embeddings is B x L x E, E is self.word_emb_dim
+        emb = self.embeddings(sent)
+        # packing for LSTM
+        packed_emb = nn.utils.rnn.pack_padded_sequence(
+            emb, sent_len.squeeze(1), batch_first=True, enforce_sorted=False
+        )
+        # out is h_t at each t. shape is B x L x 2 * H, where H is self.hid_dim
+        out_packed, (_h_t, _c_t) = self.bilstm(packed_emb)
+        # to take maxpool, need to reapply padding
+        out_padded, _sent_lens = nn.utils.rnn.pad_packed_sequence(
+            out_packed, batch_first=True
+        )
+        # we take maxpool across L to get (B x 2 * H)
+        out, _out_idxs = torch.max(out_padded, dim=1)
         return out
